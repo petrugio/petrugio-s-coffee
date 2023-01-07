59dbbe57925e88f07bfc6613a8fbe5cb3e7ac265
petrugio <petruchelban@gmail.com>
2023-01-07 16:57:18 +0000

    Migrate database to ElephantSQL

--- petrugio_s_coffee/settings.py
+++ petrugio_s_coffee/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os
 from pathlib import Path
+import dj_database_url
 
 if os.path.isfile("env.py"):
     import env
@@ -77,17 +78,9 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'petrugio_s_coffee.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+     'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
