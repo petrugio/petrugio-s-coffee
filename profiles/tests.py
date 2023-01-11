0c9e0913a8b619b99f9cac5524e1a48ed390fb0a
petrugio <petruchelban@gmail.com>
2023-01-11 21:01:52 +0000

    Add test to check if profile page and view loads

--- profiles/tests.py
+++ profiles/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.http import HttpRequest
+from django.shortcuts import get_object_or_404
 
-# Create your tests here.
+
+class TestProfileViews(TestCase):
+    """
+    Test the profile page class
+    """
+    def setUp(self):
+        """Set up a test user"""
+        username = 'tester'
+        email = 'tester@test.com'
+        password = 'password'
+        user = get_user_model()
+        self.user = user.objects.create_user(
+            username=username,
+            email=email,
+            password=password
+        )
+
+    def test_profile_page(self):
+        """Testing profile page"""
+        response = self.client.get('/profiles/')
+        self.assertEqual(response.status_code, 404)
