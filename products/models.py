748508b9db075d913ec7aa76c669b476eca3eda7
petrugio <petruchelban@gmail.com>
2023-01-17 22:17:38 +0000

    Fix product rating display decimals

--- products/models.py
+++ products/models.py
@@ -44,7 +44,7 @@ class Product(models.Model):
     intensity = models.CharField(choices=INTENSITY_CHOICES,
                                  max_length=24,
                                  default='')
-    rating = models.DecimalField(max_digits=6, decimal_places=2,
+    rating = models.DecimalField(max_digits=6, decimal_places=1,
                                  null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
