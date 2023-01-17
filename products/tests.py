ccefd949bee167251d975c8face64a965555d8f8
petrugio <petruchelban@gmail.com>
2023-01-17 16:10:27 +0000

    Add test to check if products page and view loads

--- products/tests.py
+++ products/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class TestProductsViews(TestCase):
+    """
+    Test the products page class
+    """
+    def test_products_page(self):
+        """
+        Check the products page loads
+        """
+        response = self.client.get('/products/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'products/products.html')
