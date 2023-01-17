83e497735f01d4211f1829a2608133ba07f942bd
petrugio <petruchelban@gmail.com>
2023-01-17 16:30:23 +0000

    Remove sizes and add weight to the products model

--- products/models.py
+++ products/models.py
@@ -36,7 +36,7 @@ class Product(models.Model):
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
-    has_sizes = models.BooleanField(default=False, null=True, blank=True)
+    weight = models.CharField(max_length=12, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     intensity = models.CharField(choices=INTENSITY_CHOICES,
                                  max_length=24,
