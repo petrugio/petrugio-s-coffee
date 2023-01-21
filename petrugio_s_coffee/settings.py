90f23929092939c02b878f1c9a06af911f0a672a
petrugio <petruchelban@gmail.com>
2023-01-21 19:27:56 +0000

    Add checkout functionality

--- petrugio_s_coffee/settings.py
+++ petrugio_s_coffee/settings.py
@@ -173,8 +173,12 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+# Stripe
 FREE_DELIVERY_THRESHOLD = 20
 STANDARD_DELIVERY_PERCENTAGE = 10
+STRIPE_CURRENCY = 'usd'
+STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY', '')
 
 if 'USE_AWS' in os.environ:
     # Cache control
