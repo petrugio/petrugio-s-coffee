f5fd406fd9fa053904770f37b2bc95f03219b138
petrugio <petruchelban@gmail.com>
2023-01-22 16:06:20 +0000

    Add stripe webhook

--- petrugio_s_coffee/settings.py
+++ petrugio_s_coffee/settings.py
@@ -179,6 +179,7 @@ STANDARD_DELIVERY_PERCENTAGE = 10
 STRIPE_CURRENCY = 'usd'
 STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY', '')
+STRIPE_WH_SECRET = os.environ.get('STRIPE_WH_SECRET', '')
 
 if 'USE_AWS' in os.environ:
     # Cache control
