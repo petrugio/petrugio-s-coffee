63662d278bcdb38ed920396e4ac685985fcd6585
petrugio <petruchelban@gmail.com>
2023-01-24 23:30:05 +0000

    Add blog app model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+STATUS = ((0, "Draft"), (1, "Published"))
+
+
+class Blog(models.Model):
+    """Blog model"""
+
+    title = models.CharField(max_length=254)
+    slug = models.SlugField(max_length=200, unique=True)
+    content = models.TextField()
+    image = models.ImageField(null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    likes = models.ManyToManyField(
+        User, related_name='blogpost_like', blank=True)
+
+    class Meta:
+        """Sorts blog posts in descending order"""
+        ordering = ["-created"]
+
+    def __str__(self):
+        return self.title
+
+    def number_of_likes(self):
+        return self.likes.count()
--- petrugio_s_coffee/settings.py
+++ petrugio_s_coffee/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     'products',
     'basket',
     'checkout',
+    'blog',
     'storages',
 
     'crispy_forms',
