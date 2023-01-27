1a6eefd6960230eb21fb7bf0cc4206a9b2484dd0
petrugio <petruchelban@gmail.com>
2023-01-27 13:56:53 +0000

    Add 3 most recent testimonials to testimonials section on homepage

--- home/views.py
+++ home/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
-
-# Create your views here.
+from testimonials.models import Testimonials
 
 
 def index(request):
     """ A view to return the index page """
-
-    return render(request, 'home/index.html')
+    queryset = Testimonials.objects.filter(
+        status=1).order_by("-created")[:3]
+    return render(
+        request,
+        'home/index.html',
+        {
+            'testimonials': queryset,
+        }
+    )
