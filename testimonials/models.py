5881aa699727ab63c45815310bcd296358b08033
petrugio <petruchelban@gmail.com>
2023-01-27 00:40:58 +0000

    Add slugify function to testimonial model

--- testimonials/models.py
+++ testimonials/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.text import slugify
 
 STATUS = ((0, "Draft"), (1, "Published"))
 
@@ -15,6 +16,10 @@ class Testimonials(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.IntegerField(choices=STATUS, default=0)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super(Testimonials, self).save(*args, **kwargs)
 
     class Meta:
         verbose_name_plural = 'Testimonials'
