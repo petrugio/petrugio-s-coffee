f51fd81f68e9ab24ea3ebbf4b06dcb13b3baf79b
petrugio <petruchelban@gmail.com>
2023-01-27 00:45:07 +0000

    Add testimonial view and form

---
+++ testimonials/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from django_summernote.widgets import SummernoteWidget
+from .models import Testimonials
+
+STATUS_CHOICES = ((0, "Draft"), (1, "Published"))
+
+
+class TestimonialForm(forms.ModelForm):
+    """ Form to add a testimonial """
+
+    title = forms.CharField(max_length=254)
+    content = forms.CharField(widget=SummernoteWidget())
+    status = forms.ChoiceField(choices=STATUS_CHOICES)
+
+    class Meta:
+        model = Testimonials
+        fields = ('title', 'content',
+                  'status')
