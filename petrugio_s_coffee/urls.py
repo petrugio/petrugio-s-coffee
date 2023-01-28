2b01f988c9b3910158a4db5ff09cb7231c85faa8
petrugio <petruchelban@gmail.com>
2023-01-28 12:39:32 +0000

    Add contact app urls

--- petrugio_s_coffee/urls.py
+++ petrugio_s_coffee/urls.py
@@ -29,4 +29,5 @@ urlpatterns = [
     path('summernote/', include('django_summernote.urls')),
     path('blog/', include('blog.urls'), name='blog-urls'),
     path('testimonials/', include('testimonials.urls')),
+    path('contact/', include('contact.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
